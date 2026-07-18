2b498dfa27bff9fc921772e5276ed9b301c89643
Diego Duarte <diegocavalcante040307@gmail.com>
2026-07-18 15:21:12 -0300

    feat: add entities

---
+++ src/main/java/com/diegoduarte/devlog_api/infrastructure/entities/ComentEntity.java
@@ -0,0 +1,36 @@
+package com.diegoduarte.devlog_api.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comentarios")
+public class ComentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "conteudo")
+    private String conteudo;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+}
---
+++ src/main/java/com/diegoduarte/devlog_api/infrastructure/entities/UserEntity.java
@@ -0,0 +1,32 @@
+package com.diegoduarte.devlog_api.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(name = "ativo")
+    private boolean ativo;
+}
