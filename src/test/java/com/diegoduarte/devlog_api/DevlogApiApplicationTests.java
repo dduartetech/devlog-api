df2bc0756c21ff661f8e6f47904a4696bc65f4f5
Diego Duarte <diegocavalcante040307@gmail.com>
2026-07-18 14:55:46 -0300

    commit inicial

---
+++ src/test/java/com/diegoduarte/devlog_api/DevlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoduarte.devlog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
